157b0f0668e3fc29a130514811e5def8f2a296fd
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2026-01-15 23:30:52 +0530

    rename

---
+++ system-design HLD coder notes/notes.js
@@ -0,0 +1,15 @@
+// loadbalancer-/ api gateway
+// api gateway- 1. transfer api call correct microservices
+/*
+api gateway- 1. transfer api call correct microservices
+2. its can use for authentication
+3. we can implemnt rate limiting logic to api gateway.
+
+// how api server decide which microscerce to call
+ans- service discory/ zookeeper/zulu-EUREKA
+
+
+
+
+
+*/
